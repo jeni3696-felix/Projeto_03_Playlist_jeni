3b7858640ddb9b1e8142133c961c3ff038b3fb96
Jeniffer Felix <jeniffercris3696@gmail.com>
2025-09-10 14:26:58 -0300

    Add files via upload

---
+++ spotfy.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+# Dados de exemplo
+generos = ["GOSPEL", "POP INTERNACIONAL", "TRAP", "DISNEY"]
+
+# Dicionário relacionando gêneros aos seus livros
+musica_por_genero = {
+    "GOSPEL": ["QUEM È ESSE", "EM MEIO AO VENDAVAL", "TOCA-ME"],
+    "POP INTERNACIONAL": [ "Die With A Smile","Treat you Better","WHEN|WAS YOUR MAN"],
+    "TRAP": ["SUPERMAN", "SMACK THAT", "BUTTERFLY EFFECT"],
+    "DISNEY": ["VEJO EM FIM A LUZ BRILHAR", "UM MUNDO IDEAL", "VEJO UMA PORTA ABRIR"]
+}
+
+# Selectbox para escolher o gênero                
+genero_selecionado = st.sidebar.selectbox("Selecione o gênero musical:", generos)
+
+# Selectbox para escolher o livro (apenas do gênero selecionado)
+if genero_selecionado:
+    musica_selecionada= st.sidebar.selectbox(
+        "Selecione o livro:", 
+        musica_por_genero[genero_selecionado]
+    )
+
+# Mostrar apenas o livro selecionado
+if genero_selecionado and musica_selecionada:
+    st.write(f"**MUSICA SELECIONADA:** {musica_selecionada}")
+    st.write(f"**gênero:** {genero_selecionado}")
+    # st.image(f"{musica_selecionada}.png")
+
+
+
+if genero_selecionado == "GOSPEL" and musica_selecionada == "QUEM È ESSE" :
+    st.video('https://www.youtube.com/watch?v=0ZF5em0MTwY&list=RD0ZF5em0MTwY&start_radio=1')
+
+elif genero_selecionado == "GOSPEL" and musica_selecionada == "EM MEIO AO VENDAVAL" :
+    st.video("https://www.youtube.com/watch?v=peitGcdyukA")  
+
+elif genero_selecionado == "GOSPEL" and musica_selecionada == "TOCA-ME" :
+    st.video("https://www.youtube.com/watch?v=Ul4dgaXy4Ps&list=RDUl4dgaXy4Ps&start_radio=1")  
+
+elif genero_selecionado == "POP INTERNACIONAL" and musica_selecionada == "Die With A Smile" :
+    st.video("https://www.youtube.com/watch?v=kPa7bsKwL-c&list=RDkPa7bsKwL-c&start_radio=1") 
+
+elif genero_selecionado == "POP INTERNACIONAL" and musica_selecionada == "Treat you Better" :
+    st.video("https://www.youtube.com/watch?v=lY2yjAdbvdQ&list=RDlY2yjAdbvdQ&start_radio=1")
+
+elif genero_selecionado == "POP INTERNACIONAL" and musica_selecionada == "WHEN|WAS YOUR MAN" :
+    st.video("https://www.youtube.com/watch?v=ekzHIouo8Q4&list=RDekzHIouo8Q4&start_radio=1")
+
+elif genero_selecionado == "TRAP" and musica_selecionada == "SUPERMAN" :
+    st.video("http://youtube.com/watch?v=lPlePBCS6Ic&list=RDlPlePBCS6Ic&start_radio=1")
+    
+
+
